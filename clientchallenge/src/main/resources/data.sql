fe21490b9712119ef45b61fd74b11ac0e9cb9588
Vinícius Avelino <70921839+Vinicius-Avelino@users.noreply.github.com>
2022-01-09 22:34:16 -0300

    Update data.sql

--- clientchallenge/src/main/resources/data.sql
+++ clientchallenge/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_client (name,cpf,income,birthdate,children) 	VALUES ('Vinicius', '48654934840', 6450.00, TIMESTAMP WITH TIME ZONE '2000-07-12T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name,cpf,income,birthdate,children) 	VALUES ('Vinicius', '48654934840', 3680.00, TIMESTAMP WITH TIME ZONE '2000-07-12T20:50:07.12345Z', 0);
 INSERT INTO tb_client (name,cpf,income,birthdate,children) 	VALUES ('Italo', '65835842540', 5124.00, TIMESTAMP WITH TIME ZONE '2000-11-26T20:50:07.12345Z', 2);
 INSERT INTO tb_client (name,cpf,income,birthdate,children) 	VALUES ('João', '96746734870', 3200.00, TIMESTAMP WITH TIME ZONE '2000-02-11T20:50:07.12345Z', 1);
 INSERT INTO tb_client (name,cpf,income,birthdate,children) 	VALUES ('Esdras', '38504678558', 1500.00, TIMESTAMP WITH TIME ZONE '2000-11-24T20:50:07.12345Z', 0);
